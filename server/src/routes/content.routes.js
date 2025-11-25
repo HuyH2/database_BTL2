5e7b4f2af6749b3a2307b2b7ddcdaf59cd0ac29f
Nguyet Ha <ha.dinhnguy2005@hcmut.edu.vn>
2025-11-25 23:10:41 +0700

    Sưa lai chi còn client

--- server/src/routes/content.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const ctrl = require('../controllers/content.controller')
-
-router.get('/', ctrl.list)
-router.get('/:id', ctrl.detail)
-
-module.exports = router
--- server/src/routes/db.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const ctrl = require('../controllers/db.controller')
-
-router.post('/init', ctrl.init)
-router.get('/check', ctrl.check)
-router.post('/clear', ctrl.clear)
-
-module.exports = router
